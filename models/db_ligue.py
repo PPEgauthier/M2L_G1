769accfaf8d4b81f65a3cf981e3eea45074423ca
unknown <lgoubin@DA13.sio-coliniere44.local>
2015-11-04 14:30:51 +0100

    maj du dal

--- models/db_ligue.py
+++ models/db_ligue.py
@@ -7,4 +7,38 @@ db.define_table('ligue',
              Field('tel','string',requires=IS_NOT_EMPTY()),
              Field('URLSiteWeb','string'),
              Field('emailContact','string')  
-             ,migrate=False)
+             ,migrate=False
+               )
+
+db.define_table('adherent',
+             Field('nom','string',requires=IS_NOT_EMPTY()),
+             Field('prenom','string',requires=IS_NOT_EMPTY()),
+             Field('dateNaissance','string',requires=IS_NOT_EMPTY()),
+             Field('sexe','string',requires=IS_NOT_EMPTY()),
+             Field('adresseRue','string',requires=IS_NOT_EMPTY()),
+             Field('ville','string',requires=IS_NOT_EMPTY()),
+             Field('cp','string',requires=IS_NOT_EMPTY()),
+             Field('numLicence','string',requires=IS_NOT_EMPTY()),
+             Field('tel','string',requires=IS_NOT_EMPTY()),
+             Field('email','string')
+             ,Field('idLigue','reference ligue',requires=IS_IN_DB(db,db.ligue.id))
+             ,migrate=True,fake_migrate=True
+               )
+
+db.define_table('formation',
+             Field('libelle','string',requires=IS_NOT_EMPTY()),
+             Field('dateDeb','datetime',requires=IS_DATETIME()),
+             Field('dateFin','datetime',requires=IS_DATETIME()),
+             Field('nbPlaces','string',requires=IS_NOT_EMPTY()),
+             Field('dateEcheance','datetime',requires=IS_DATETIME())
+             ,migrate=True,fake_migrate=True
+                )
+
+db.define_table('inscription',
+             #Field('numLicence','reference licence',requires=IS_IN_DB(db.db.licence.id)),
+             Field('idFormation','reference formation',requires=IS_IN_DB(db,db.formation.id)),
+             Field('idAdherent','reference adherent',requires=IS_IN_DB(db,db.adherent.id)),
+             Field('dateInscription','datetime',requires=IS_DATETIME()),
+             Field('champsFormulaire','string',requires=IS_NOT_EMPTY()),
+             migrate=True, fake_migrate=True
+                )
